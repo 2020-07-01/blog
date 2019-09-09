146752d723fe13b3240cbd3b793a9bcc26a4dcfa
yq-java <2422321558@qq.com>
2019-09-09 15:58:50 +0800

    cookie的验证机制

---
+++ src/main/java/com/example/blog/security/CookiePower.java
@@ -0,0 +1,20 @@
+package com.example.blog.security;
+
+import com.example.blog.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * @author :qiang
+ * @date :2019/9/8 上午10:44
+ * @description :
+ * @other :
+ */
+public class CookiePower {
+
+    //获取所有用户名
+    @Autowired
+    UserService userService;
+
+
+
+}
