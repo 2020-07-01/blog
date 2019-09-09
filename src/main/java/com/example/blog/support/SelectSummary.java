cf6d3bcf8ea357270c71cdec25ca655d1bca56b5
yq-java <2422321558@qq.com>
2019-09-09 16:00:00 +0800

    概要内容转换

---
+++ src/main/java/com/example/blog/support/SelectSummary.java
@@ -0,0 +1,22 @@
+package com.example.blog.support;
+
+import com.example.blog.entity.Article;
+
+import java.util.List;
+
+/**
+ * @author :qiang
+ * @date :2019/9/8 下午7:25
+ * @description :Summary字段的规范
+ * @other :
+ */
+public class SelectSummary {
+
+    //创建字符数组
+    String[] string = {"#", "##", "###", "####", "```"};
+
+    private void summary(List<Article> articleList) {
+
+    }
+
+}
